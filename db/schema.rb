f1a0fd56d1b1b4471e01c4c999f8fecf0635b6f6
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-10-06 15:01:38 +0100

    Add student model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_114540) do
+ActiveRecord::Schema.define(version: 2020_10_06_134136) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
