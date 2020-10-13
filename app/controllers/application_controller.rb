6d242673afe243148530e567caf7fed384d3deac
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-10-13 18:00:31 +0100

    Create and destroy sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in?
+  def current_user
+    # ||= memoization only perform the query if @current_user does not exists
+    @current_user ||= Student.find(session[:student_id]) if session[:student_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+
 end
