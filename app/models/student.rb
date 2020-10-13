26ddf5cb51fcd8a0fe9f50f83e322cdeffaf5971
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-10-13 17:01:15 +0100

    Add secure password

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,5 +6,7 @@ class Student < ApplicationRecord
   validates :email, presence: true, length: {maximum: 105},
             uniqueness: {case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX}
+
+  has_secure_password
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_134136) do
+ActiveRecord::Schema.define(version: 2020_10_13_155512) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_10_06_134136) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
